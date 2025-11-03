9e6e6ae27721ff7b455c8bafc80e88251db7929a
Paloma <167933050+PalomaContreras25@users.noreply.github.com>
2025-11-03 15:42:50 -0500

    upd_report

--- analysis/render_all.R
+++ analysis/render_all.R
@@ -3,22 +3,37 @@
 use_here <- requireNamespace("here", quietly = TRUE)
 h <- if (use_here) here::here else function(...) file.path(...)
 
-
+# List of plates to render
 plates <- paste0("Test", 7:10)
+
+# Define which spike standard is used for each plate
+spike_lookup <- data.frame(
+  plate = c("Test7", "Test8", "Test9", "Test10"),
+  spike_std = c("st1", "st1", "st4", "std1"),
+  stringsAsFactors = FALSE
+)
 
 render_one <- function(plate) {
   input    <- h("analysis", "Data_analysis.Rmd")
-  out_dir  <- h("data", plate)
+  out_dir  <- h("output", plate)
   out_file <- paste0(plate, "_plate_report.html")
+  
+  # look up the spike for this plate
+  spike_std <- spike_lookup$spike_std[match(plate, spike_lookup$plate)]
   
   rmarkdown::render(
     input       = input,
-    params      = list(plate = plate, data_root = h("data")),
+    params      = list(
+      plate = plate,
+      spike_std = spike_std,   # new parameter!
+      data_root = h("data")
+    ),
     output_file = out_file,
     output_dir  = out_dir,
-    # either omit envir entirely (recommended) or:
     envir       = new.env(parent = globalenv())
   )
 }
 
+# Run rendering for all plates
 invisible(lapply(plates, render_one))
+
